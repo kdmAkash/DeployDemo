a4f9aacbbbd8a35774671feda8f8c867a780d66b
kdmAkash <arkadam2018@gmail.com>
2025-04-08 23:28:06 +0530

    Use dynamic port for Railway

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ app.get("/get",(req,res)=>{
         console.log(result);
     })
 })
+const PORT = process.env.PORT || 3000;
 
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
